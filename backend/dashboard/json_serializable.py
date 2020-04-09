8b0fa7c3abac6a51c06c55710af5f8693f6bbcd6
sud0su <rencongpc@gmail.com>
2020-04-10 01:25:21 +0700

    update filtering on dashboard page

--- backend/dashboard/json_serializable.py
+++ backend/dashboard/json_serializable.py
@@ -1,5 +1,5 @@
 import json
-from django.db.models import Sum, Q
+from django.db.models import Sum, Q, Count
 from django.db.models.functions import Coalesce
 from .models import Undss
 from reference.models import IncidentType, Province, District
@@ -18,10 +18,9 @@ def MainData(request):
 
     return main
 
-def Chart(request, code, daterange):
+def Chart(request, code, daterange, incident_type):
     main = MainData(request)
     chart = {}
-
     undssQueryset =  Undss.objects.all()
     if code:
         getCode = code.split('=')
@@ -33,6 +32,10 @@ def Chart(request, code, daterange):
     if daterange:
         date = daterange.split(',')
         undssQueryset = undssQueryset.filter(Date__gte=date[0],Date__lte=date[1])
+
+    if incident_type:
+        undssQueryset = undssQueryset.filter(Incident_Type__name__in=incident_type.split(','))
+
 
     ## Bar Chart
     chart['bar_chart'] = {}
@@ -82,7 +85,7 @@ def Chart(request, code, daterange):
 
     return chart
 
-def Table(request, code, daterange):
+def Table(request, code, daterange, incident_type):
     table = {}
     main = MainData(request)
 
@@ -98,17 +101,24 @@ def Table(request, code, daterange):
         date = daterange.split(',')
         undssQueryset = undssQueryset.filter(Date__gte=date[0],Date__lte=date[1])
 
+    if incident_type:
+        print(incident_type)
+        undssQueryset = undssQueryset.filter(Incident_Type__name__in=incident_type.split(','))
+
     # list_of_latest_incidents
     table['list_of_latest_incidents'] = undssQueryset.values('Date', 'Time_of_Incident','Description_of_Incident').order_by('-Date')
     
     # total killed, injure, abducted group by incident type
     incidentTypeData = []
     incidentTypeName = []
+    incidentCountData = []
     for pc in main["category"]:
-        incident_type_data = undssQueryset.values('Incident_Type__name').annotate(total = Coalesce(Sum(pc), 0)).order_by('-Incident_Type_id')
+        incident_type_data = undssQueryset.values('Incident_Type__name').annotate(count = Coalesce(Count("Incident_Type_id"), 0),total = Coalesce(Sum(pc), 0)).order_by('-Incident_Type_id')
+        total_incident = [total['count'] for total in incident_type_data] 
         total_result = [total['total'] for total in incident_type_data]
         incident_name = [total['Incident_Type__name'] for total in incident_type_data]
         incidentTypeData += [total_result]
+        incidentCountData += [total_incident]
         incidentTypeName += [incident_name]
 
     # incidents_and_casualties_by_incident_type
@@ -119,7 +129,8 @@ def Table(request, code, daterange):
             'killed': incidentTypeData[0][i],
             'injured': incidentTypeData[1][i],
             'abducted': incidentTypeData[2][i],
-            'total': incidentTypeData[0][i] + incidentTypeData[1][i] + incidentTypeData[2][i]
+            # 'total': incidentTypeData[0][i] + incidentTypeData[1][i] + incidentTypeData[2][i]
+            'total': incidentCountData[0][i]
         }
         table_incident_type_total.append(table_data)
     
@@ -127,32 +138,40 @@ def Table(request, code, daterange):
 
     # total killed, injure, abducted group by province, and country
     countryData = []
+    countryIncident = []
     for pc in main["category"]:
-        country_data = undssQueryset.values(pc).annotate(total = Coalesce(Sum(pc), 0)).order_by('-'+pc)
+        country_data = Undss.objects.all().values(pc).annotate(count = Coalesce(Count("Incident_Type_id"), 0), total = Coalesce(Sum(pc), 0)).order_by('-'+pc)
+        total_incident = [total['count'] for total in country_data] 
         total_result = [int(total['total']) for total in country_data]
+        countryIncident += [sum(total_incident)]
         countryData += [sum(total_result)]
 
     provinceData = []
     provinceName = []
+    provinceIncident = []
     for pc in main["category"]:
-        province_data = undssQueryset.values('Province__name').annotate(total = Coalesce(Sum(pc), 0)).order_by('-Province_id')
+        province_data = undssQueryset.values('Province__name').annotate(count = Coalesce(Count("Incident_Type_id"), 0), total = Coalesce(Sum(pc), 0)).order_by('-Province_id')
+        total_incident = [total['count'] for total in province_data] 
         total_result = [total['total'] for total in province_data]
         province_name = [total['Province__name'] for total in province_data]
         provinceData += [total_result]
+        provinceIncident += [total_incident]
         provinceName += [province_name]
-
 
     # number_of_incident_and_casualties_overview
     parentname = ['Afghanistan']
     table['number_of_incident_and_casualties_overview'] = {}
-    countryDataParent = parentname + countryData + [sum(countryData)]
+    countryDataParent = parentname + [countryIncident[0]] + [countryData[2]] + [countryData[1]] + [countryData[0]]
 
     table['number_of_incident_and_casualties_overview']['parentdata'] = [countryDataParent]
     table['number_of_incident_and_casualties_overview']['child'] = []
     countryDataChild = []
     for i in range(0, len(provinceName[0])):
-        tot = provinceData[0][i] + provinceData[1][i] + provinceData[2][i]
-        countryDataChild = [provinceName[0][i]] + [provinceData[0][i]] + [provinceData[1][i]] + [provinceData[2][i]] + [tot]
+        if provinceName[0][i] == 'NULL':
+            provinceName[0][i] = 'NoName'
+        # tot = provinceData[0][i] + provinceData[1][i] + provinceData[2][i]
+        # countryDataChild = [provinceName[0][i]] + [provinceData[0][i]] + [provinceData[1][i]] + [provinceData[2][i]] + [tot]
+        countryDataChild = [provinceName[0][i]] + [provinceIncident[0][i]] +[provinceData[2][i]] + [provinceData[1][i]] + [provinceData[0][i]]
         # table['number_of_incident_and_casualties_overview']['child'].append({'name':provinceName[0][i].replace(" ", "_").lower(), 'value': countryDataChild})
         table['number_of_incident_and_casualties_overview']['child'].append({'name':provinceName[0][i], 'value': countryDataChild})
     table['number_of_incident_and_casualties_overview']['key'] = "number_of_incident_and_casualties_overview"
@@ -187,7 +206,7 @@ def Region(request, code):
     region["district"].append({"data_val" : getDistrict, "selected": distname, "type" : "District", "urlcode": "dist"})
     return region
 
-def Total(request, code, daterange):
+def Total(request, code, daterange, incident_type):
     total = {}
     main = MainData(request)
 
@@ -203,34 +222,41 @@ def Total(request, code, daterange):
         date = daterange.split(',')
         undssQueryset = undssQueryset.filter(Date__gte=date[0],Date__lte=date[1])
 
+    if incident_type:
+        undssQueryset = undssQueryset.filter(Incident_Type__name__in=incident_type.split(','))
+
     countryData = []
     totalCountry = []
     for pc in main["category"]:
-        country_data = undssQueryset.values(pc).annotate(total = Coalesce(Sum(pc), 0)).order_by('-'+pc)
+        country_data = undssQueryset.values(pc).annotate(count = Coalesce(Count("Incident_Type_id"), 0), total = Coalesce(Sum(pc), 0)).order_by('-'+pc)
+        total_incident = [int(total['count']) for total in country_data]
         total_result = [int(total['total']) for total in country_data]
         totalCountry += [sum(total_result)]
         countryData.append({pc: sum(total_result)})
 
     provinceData = []
     provinceName = []
+    provinceIncident = []
     for pc in main["category"]:
-        province_data = undssQueryset.values('Province__name').annotate(total = Coalesce(Sum(pc), 0)).order_by('-Province_id')
+        province_data = undssQueryset.values('Province__name').annotate(count = Coalesce(Count("Incident_Type_id"), 0),total = Coalesce(Sum(pc), 0)).order_by('-Province_id')
+        total_incident = [total['count'] for total in province_data] 
         total_result = [total['total'] for total in province_data]
         province_name = [total['Province__name'] for total in province_data]
         provinceData += [total_result]
+        provinceIncident += [total_incident]
         provinceName += [province_name]
 
     countryDataChild = []
     for i in range(0, len(provinceName[0])):
-        tot = provinceData[0][i] + provinceData[1][i] + provinceData[2][i]
+        # tot = provinceData[0][i] + provinceData[1][i] + provinceData[2][i]
         countryDataChild.append({
             'killed': provinceData[0][i],
             'injured': provinceData[1][i],
             'abducted': provinceData[2][i],
-            'total' : tot
+            'incident' : provinceIncident[0][i]
         })
     
-    countryData.append({'total': sum(totalCountry)})
+    countryData.append({'incident': sum(total_incident)})
 
     if code:
         total['total_data'] = countryDataChild
@@ -239,28 +265,40 @@ def Total(request, code, daterange):
 
     return total  
 
-def DashboardResponse(request, code, daterange):
+def DashboardResponse(request, code, daterange, incident_type):
     dashboardresponse = {}
-    chart = Chart(request, code, daterange)
-    table = Table(request, code, daterange)
-    total = Total(request, code, daterange)
+    main = MainData(request)
+    chart = Chart(request, code, daterange, incident_type)
+    table = Table(request, code, daterange, incident_type)
+    total = Total(request, code, daterange, incident_type)
     region = Region(request, code)
     dashboardresponse["chart"] = chart
     dashboardresponse["tables"] = table
     dashboardresponse["region"] = region
     dashboardresponse["total"] = total
+    dashboardresponse["incident_type"] = {}
+    dashboardresponse["incident_type"]["name"] = main["incident_type_name"]
+    if not incident_type or incident_type == "0":
+        dashboardresponse["incident_type"]["checked"] =  main["incident_type_name"]
+    else:
+        dashboardresponse["incident_type"]["checked"] = incident_type.split(',')
+    
     return dashboardresponse
 
 def Common(request):
     response = {}
     code = None
     daterange = None
+    incident_type = ""
 
     if 'code' in request.GET:
         code = request.GET['code']
 
     if 'daterange' in request.GET:
         daterange = request.GET['daterange']
+    
+    if 'incident_type' in request.GET:
+        incident_type = request.GET['incident_type']
 
     if 'page' not in request.GET:
         mutable = request.GET._mutable
@@ -269,8 +307,8 @@ def Common(request):
         request.GET._mutable = mutable
 
     if request.GET['page'] == 'dashboard':
-        response = DashboardResponse(request, code, daterange)
-        
+        response = DashboardResponse(request, code, daterange, incident_type)
+         
     response['jsondata'] = json.dumps(response, cls=JSONEncoderCustom)
 
     return response
--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -1,5 +1,9 @@
+import pdfkit
 from django.shortcuts import render, redirect
-from django.template import RequestContext
+# from django.template import RequestContext
+from django.template.loader import get_template 
+# from django.template import Context
+
 from django.contrib.auth.decorators import login_required
 
 from .forms import UndssForm
@@ -31,12 +35,36 @@ def InputDashboard(request):
 
 @login_required
 def Dashboard(request):
-    
     if not request.GET.get('page'):
         currenturl = request.build_absolute_uri()
         return redirect(replace_query_param(currenturl, 'page', 'dashboard'))
 
     response = Common(request)
-    # template = "dashboard/dashboard_base.html"
     template = "dashboard/dashboard_content.html"
+
+    if 'pdf' in request.GET:
+        options = {
+            'page-size': 'A4',
+            'margin-top': '0.75in',
+            'margin-right': '0.75in',
+            'margin-bottom': '0.75in',
+            'margin-left': '0.75in',
+        }
+        template = get_template(template)
+        # context = Context(response)  # data is the context data that is sent to the html file to render the output. 
+        html = template.render(response)  # Renders the template with the context data.
+        pdfkit.from_string(html, 'out.pdf')
+        pdf = open("out.pdf")
+        response = HttpResponse(pdf.read(), content_type='application/pdf')  # Generates the response as pdf response.
+        response['Content-Disposition'] = 'attachment; filename=output.pdf'
+        pdf.close()
+        os.remove("out.pdf")  # remove the locally created pdf file.
+        return response  # returns the response
+
     return render(request, template, response)
+
+# @login_required
+# def DashboardPrint(request):
+#     response = Common(request)
+#     # template = ""
+#     pass
