35dddef5c83aa32b3d8c40dd374b48d2fde6b399
sud0su <ahmat.raz@gmail.com>
2020-11-17 12:19:53 +0700

    remove sqlite database setting

--- backend/giz/settings.py
+++ backend/giz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yxl)$t2l%kt_-m)%89nz=l0lwg^dwly^#frv-ov%v8&)bh65%o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'giz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
